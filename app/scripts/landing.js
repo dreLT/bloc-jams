b70b2dc85639f221803384c29438aec59ed19769
Andre Leith-Tetrault <andre.leith@gmail.com>
2014-10-05 17:09:17 +0000

    Added jQuery animations to landing page

---
+++ app/scripts/landing.js
@@ -0,0 +1,20 @@
+$(document).ready(function() { 
+   $('.hero-content h3').click(function(){
+      subText = $(this).text();
+      $(this).text(subText + '!');
+   });
+
+   var onHoverAction = function() {
+    console.log('Hover action triggered');
+    $(this).animate({'margin-top': '10px'});
+   };
+
+   var offHoverAction = function() {
+    console.log('Off-hover action triggered');
+    $(this).animate({'margin-top': '0px'});
+   };
+
+   $('.selling-points .point').hover(onHoverAction, offHoverAction);
+
+});
+
