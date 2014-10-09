10cb6c4b90a1605458e9bca3823de11e3058ca00
Andre Leith-Tetrault <andre.leith@gmail.com>
2014-10-09 21:00:43 +0000

    Added an overlay to the collection view

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -1,7 +1,9 @@
 var buildAlbumThumbnail = function() {
   var template =
    '<div class="collection-album-container col-md-2">'
- + '  <img src="/images/album-placeholder.png"/>'
+ + '  <div class="collection-album-image-container">'
+ + '    <img src="/images/album-placeholder.png"/>'
+ + '  </div>'
  + '  <div class="caption album-collection-info">'
  + '    <p>'
  + '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -21,24 +23,38 @@ var updateCollectionView = function() {
   var $collection = $('.collection-container .row')
   $collection.empty();
 
-// Attempt at randomizing output. To ask Phil //  
-
-//   var randomizedAlbumOutput = function() {
-//     return Math.floor(Math.random() * (100 - 25)) + 25;
-//   };
-
-//   console.log(randomizedAlbumOutput());
-  
-//   for (i = 0; i < randomizedAlbumOutput(); i++) {
-//     var $newThumbnail = buildAlbumThumbnail();
-//     $collection.append($newThumbnail);
-//   };
-// };
-
   for (i = 0; i < 33; i++) {
     var $newThumbnail = buildAlbumThumbnail();
     $collection.append($newThumbnail);
   };
+
+  var onHover = function(event) {
+    $(this).append(buildAlbumOverlay("/album.html"));
+  };
+
+  var offHover = function(event) {
+    $(this).find('.collection-album-image-overlay').remove();
+  };
+
+  $collection.find('.collection-album-image-container').hover(onHover, offHover);
+
+};
+
+var buildAlbumOverlay = function(albumURL) {
+  var template =
+      '<div class="collection-album-image-overlay">'
+    + '  <div class="collection-overlay-content">'
+    + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+    + '      <i class="fa fa-play"></i>'
+    + '    </a>'
+    + '    &nbsp;'
+    + '    <a class="collection-overlay-button">'
+    + '      <i class="fa fa-plus"></i>'
+    + '    </a>'
+    + '  </div>'
+    + '</div>'
+    ;
+  return $(template);
 };
 
 if (document.URL.match(/\/collection.html/)) {
