0901ae8dea4fc6024f8e3f35ad79a72c200f0b4f
Andre Leith-Tetrault <andre.leith@gmail.com>
2014-10-17 18:26:57 +0000

    Added functionality to player controls

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -208,9 +208,48 @@ var changeAlbumView = function(album) {
   }
 };
 
+var updateSeekPercentage = function($seekBar, event) {
+  var barWidth = $seekBar.width();
+  var offsetX = event.pageX - $seekBar.offset().left;
+
+  var offsetXPercent = (offsetX  / barWidth) * 100;
+  offsetXPercent = Math.max(0, offsetXPercent);
+  offsetXPercent = Math.min(100, offsetXPercent);
+
+  var percentageString = offsetXPercent + '%';
+  $seekBar.find('.fill').width(percentageString);
+  $seekBar.find('.thumb').css({left: percentageString});
+}
+
+var setupSeekBars = function() {
+
+  $seekBars = $('.player-bar .seek-bar');
+  $seekBars.click(function(event) {
+    updateSeekPercentage($(this), event);
+  });
+
+    $seekBars.find('.thumb').mousedown(function(event){
+    var $seekBar = $(this).parent();
+ 
+    $seekBar.addClass('no-animate');
+
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+ 
+    //cleanup
+    $(document).bind('mouseup.thumb', function(){
+      $seekBar.removeClass('no-animate');
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+  });
+};
+
 if (document.URL.match(/\/album.html/)) {
   $(document).ready(function() {
    changeAlbumView(albumPicasso);
+   setupSeekBars();
   });
 };
 });
