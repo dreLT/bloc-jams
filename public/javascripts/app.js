8e3bc82fb977a774b157f687abe1a8587d40ccb8
Andre Leith-Tetrault <andre.leith@gmail.com>
2014-10-19 19:27:41 +0000

    Converted landing page to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -255,11 +255,30 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require('./collection');
-require('./album');
-require('./profile');
+// require("./landing");
+// require('./collection');
+// require('./album');
+// require('./profile');
 
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.subText = "Turn the music up!";
+
+  $scope.subTextClicked = function() {
+    $scope.subText += '!';
+  };
+
+    $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+    ];
+ }]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
