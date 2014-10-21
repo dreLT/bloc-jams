aeaad2f16d4a9953a3bfad1066a65a567da990a7
Andre Leith-Tetrault <andre.leith@gmail.com>
2014-10-21 20:39:23 +0000

    Converted album view to Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,6 +30,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
    templateUrl: '/templates/collection.html'
  });
 
+ $stateProvider.state('album', {
+    url: '/album',
+    templateUrl: '/templates/album.html',
+    controller: 'Album.controller'
+ });
+
 }]);
 
 blocJams.controller('Landing.controller', ['$scope', function($scope) {
@@ -58,4 +64,43 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
   for (var i = 0; i < 33; i++) {
     $scope.albums.push(angular.copy(albumPicasso));
   }
+}]);
+
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+  $scope.album = angular.copy(albumPicasso);
+
+  var hoveredSong = null;
+  var playingSong = null;
+ 
+  $scope.onHoverSong = function(song) {
+    hoveredSong = song;
+  };
+
+  $scope.offHoverSong = function(song) {
+    hoveredSong = null;
+  };
+
+    $scope.getSongState = function(song) {
+     if (song === playingSong) {
+       return 'playing';
+     }
+     else if (song === hoveredSong) {
+       return 'hovered';
+     }
+     return 'default';
+    };
+
+    $scope.playSong = function(song) {
+      playingSong = song;
+    };
+ 
+    $scope.pauseSong = function(song) {
+      playingSong = null;
+    };
+  
+  // $scope.oddSongs = function() {
+  //   if ($odd === true) {
+  //     return 'background-color: white;';
+  //   }
+
 }]);
