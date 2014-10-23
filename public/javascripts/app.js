898a76c1b5d72dc12e1f12c46eab271f03b1d370
Andre Leith-Tetrault <andre.leith@gmail.com>
2014-10-23 17:47:47 +0000

    Made next and previous buttons on player-bar work

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -371,6 +371,11 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
     $scope.pauseSong = function(song) {
       SongPlayer.pause();
     };
+  
+  // $scope.oddSongs = function() {
+  //   if ($odd === true) {
+  //     return 'background-color: white;';
+  //   }
 
 }]);
 
