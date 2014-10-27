753c24b8760669433760bbfd3ad0af87e0920091
Andre Leith-Tetrault <andre.leith@gmail.com>
2014-10-27 16:39:11 +0000

    Changed slider to update (music location) data on change.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -262,11 +262,11 @@ var albumPicasso = {
  year: '1881',
  albumArtUrl: '/images/album-placeholder.png',
  songs: [
-     { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-     { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-     { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-     { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-     { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+    { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+    { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+    { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+    { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+    { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
    ]
 };
 
@@ -426,6 +426,11 @@ blocJams.service('SongPlayer', function() {
      var song = this.currentAlbum.songs[currentTrackIndex];
      this.setSong(this.currentAlbum, song);
    },
+   seek: function(time) {
+    if(currentSoundFile) {
+      currentSoundFile.setTime(time);
+    }
+   },
    setSong: function(album, song) {
      if (currentSoundFile) {
       currentSoundFile.stop();
@@ -453,39 +458,67 @@ var calculateSliderPercentFromMouseEvent = function($slider, event) {
   return offsetXPercent;
 }
 
+var numberFromValue = function(value, defaultValue) {
+ if (typeof value === 'number') {
+   return value;
+ }
+
+ if(typeof value === 'undefined') {
+   return defaultValue;
+ }
+
+ if(typeof value === 'string') {
+   return Number(value);
+ }
+}
+
 return {
   templateUrl: '/templates/directives/slider.html',
   replace: true,
   restrict: 'E',
-  scope: {},
+  scope: {
+    onChange: '&'
+  },
   link: function(scope, element, attributes) {
     
+    scope.value = 0;
+    scope.max = 100;
     var $seekBar = $(element);
+
+    console.log(attributes);
+    attributes.$observe('value', function(newValue) {
+      scope.value = numberFromValue(newValue, 0);
+    });
+
+    attributes.$observe('max', function(newValue) {
+      scope.max = numberFromValue(newValue, 100) || 100;
+    });
 
     scope.onClickSlider = function(event) {
       var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
       scope.value = percent * scope.max;
+      notifyCallback(scope.value);
     }
 
     scope.trackThumb = function() {
       $document.bind('mousemove.thumb', function(event){
         var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
-        scope.$apply(function(){
+        scope.$apply(function() {
           scope.value = percent * scope.max;
+          notifyCallback(scope.value);
         });
       });
  
-    $document.bind('mouseup.thumb', function(){
-      $document.unbind('mousemove.thumb');
-      $document.unbind('mouseup.thumb');
-    });
-  };
-
-    scope.value = 0;
-    scope.max = 200;
+      $document.bind('mouseup.thumb', function(){
+        $document.unbind('mousemove.thumb');
+        $document.unbind('mouseup.thumb');
+      });
+    };
       
       var percentString = function () {
-        percent = Number(scope.value) / Number(scope.max)  * 100;
+        var value = scope.value || 0;
+        var max = scope.max || 100;
+        percent = value / max * 100;
         return percent + "%";
       }
 
@@ -495,7 +528,14 @@ return {
 
       scope.thumbStyle = function() {
         return {left: percentString()};
-      }    
+      }  
+
+      var notifyCallback = function(newValue) {
+         if(typeof scope.onChange === 'function') {
+           scope.onChange({value: newValue});
+        }
+      };
+
     }
   };
 }]);
